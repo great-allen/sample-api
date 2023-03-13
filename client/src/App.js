c60cb0f9718b21645e73638e19c2bc640835e150
QunFan <allenfan1012@gmail.com>
2023-03-13 22:48:37 +1100

    mimor changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,48 @@
-import {useEffect, useState} from 'react'
-import './App.css';
-// import { v4 as uuid } from "uuid";
+import { useEffect, useState } from "react";
+import "./App.css";
+
 function App() {
-  const [images,setImages]=useState([])
-  useEffect(()=>{
-    fetch('/images').then(r=>r.json()).then(setImages)
-  },[])
-  const[file,setFile]=useState('')
-  const handleSubmit=(e)=>{
-    e.preventDefault()
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    fetch("/images")
+      .then((r) => r.json())
+      .then(setImages);
+  }, []);
+
+  const [file, setFile] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
     const formData = new FormData();
-    formData.append('image', e.target.image.files[0]);
-    fetch('/images', {
-      method: 'POST',
+    formData.append("image", e.target.image.files[0]);
+    fetch("/images", {
+      method: "POST",
       body: formData,
     })
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         console.log(data);
+        setImages([...images, data]); // add the new image to the list
+        setFile(""); // clear the input value
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error);
       });
-
-  }
+  };
 
   return (
     <div className="App">
       <form onSubmit={handleSubmit}>
-        <input type="file" name="image" value={file} onChange={(e)=>setFile(e.target.value)}></input>
-        <button type="submit">upload</button>
+        <input type="file" name="image" onChange={(e) => setFile(e.target.value)} />
+        <button type="submit" disabled={!file}>
+          Upload
+        </button>
       </form>
-      {images&&images.map((image)=>{
-        return <img key={image.id} src={image.image_url} alt=" "/>
-      })}
+      {images &&
+        images.map((image) => {
+          return <img key={image.id} src={image.image_url} alt=" " />;
+        })}
     </div>
   );
 }
